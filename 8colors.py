cf78c9732005e31ffd801228da5f9e78c4c08481
Daiyaan <daiyaan.villain@gmail.com>
2021-08-01 01:06:29 +0600

    Print 8 colors (base)

--- 8colors.py
+++ 8colors.py
@@ -1,6 +1,6 @@
 print("\n")
 for i in range(0,8):
-    for j in range(2, 8):
-        result = (6*i) + j
+    for j in range(0, 8):
+        result = (8*i) + j
         print(f"\033[4{i};3{j}m","{:0>3d}".format(result), end = ' ')
     print("\n")
