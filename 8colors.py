b8a6c05d92bf4fecbb6436eb0fb4e00db7f9623c
Daiyaan <daiyaan.villain@gmail.com>
2021-08-01 08:38:46 +0600

    Slight changes

--- 8colors.py
+++ 8colors.py
@@ -3,4 +3,4 @@ for i in range(0,8):
     for j in range(0, 8):
         result = (8*i) + j
         print(f"\033[4{i};3{j}m","{:0>3d}".format(result), end = ' ')
-    print("\n")
+    print("\n", end='')
